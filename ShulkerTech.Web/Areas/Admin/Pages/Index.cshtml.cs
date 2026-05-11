43cb4a62e73447afefc526acf7fc2fcaf5481f6d
Trevor Koenig <trevorkoenig4@gmail.com>
2026-05-10 21:41:49 -0600

    updates to admin page

--- ShulkerTech.Web/Areas/Admin/Pages/Index.cshtml.cs
+++ ShulkerTech.Web/Areas/Admin/Pages/Index.cshtml.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ShulkerTech.Core.Data;
 using ShulkerTech.Core.Models;
+using ShulkerTech.Web.Services;
 
 namespace ShulkerTech.Web.Areas.Admin.Pages;
 
 public class IndexModel(
     ApplicationDbContext db,
-    UserManager<ApplicationUser> userManager) : PageModel
+    UserManager<ApplicationUser> userManager,
+    PermissionService permissions) : PageModel
 {
     public int TotalUsers { get; set; }
     public int ActiveInviteCodes { get; set; }
@@ -16,6 +18,8 @@ public class IndexModel(
     public int ActiveMapServers { get; set; }
     public int GameServers { get; set; }
     public int ActiveSessions { get; set; }
+
+    public HashSet<string> AccessibleResources { get; private set; } = [];
 
     public async Task OnGetAsync()
     {
@@ -32,5 +36,22 @@ public class IndexModel(
 
         GameServers    = await db.MinecraftServers.CountAsync(s => s.IsActive);
         ActiveSessions = await db.PlayerSessions.CountAsync(s => s.LeftAt == null);
+
+        var user  = await userManager.GetUserAsync(User);
+        var roles = user is not null ? await userManager.GetRolesAsync(user) : [];
+
+        string[] adminResources =
+        [
+            SiteResource.AdminUsers,         SiteResource.AdminRoles,
+            SiteResource.AdminInvites,       SiteResource.AdminServers,
+            SiteResource.AdminMaps,          SiteResource.AdminWikiSettings,
+            SiteResource.AdminWikiTags,      SiteResource.AdminWikiTemplates,
+            SiteResource.AdminSiteSettings,  SiteResource.AdminSecurity,
+            SiteResource.AdminDbExport,      SiteResource.AdminAuditLog,
+        ];
+
+        foreach (var resource in adminResources)
+            if (await permissions.CanAccessAsync(user, roles, resource, isPublicByDefault: false))
+                AccessibleResources.Add(resource);
     }
 }
